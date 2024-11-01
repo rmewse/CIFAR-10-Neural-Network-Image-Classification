1245cf4050a58f10504230178ebd5b50c97cd56b
Rebecca Mewse <wkcg77@durham.ac.uk>
2024-11-01 19:28:07 +0000

    Trained first model

--- src/model.py
+++ src/model.py
@@ -1,9 +1,21 @@
 import tensorflow as tf
-from tensorflow.keras import datasets
+from tensorflow.keras import datasets, layers, models
 
 # This project uses the CIFAR-10 dataset, load the dataset
-(train_images, train_labels), (test_images, test_labels) = dataset.cirfar10.load_data()
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Normalise all pixel values to be in range 0-1
 test_images = test_images.astype("float32") / 255.0
 train_images = test_images.astype("float32") / 255.0
+
+def init_model():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10, activation='softmax'))
+    return model
---
+++ src/train.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets
+from model import init_model #importing function from elsewhere in project
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+test_images = test_images / 255.0
+train_images = train_images / 255.0
+
+# Create and compile model
+
+model = init_model()
+
+model.compile(metrics=['accuracy'],
+              loss='sparse_categorical_crossentropy',
+              optimizer='adam')
+
+# Training model
+
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+model.save("cirfar10_model.h5")
+
+
+
+
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
